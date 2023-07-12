f5d543fd5ffa7b513ba51dfbd1993b3ec642a6c3
ZarathustraSama <mr.gabriel@live.it>
2023-07-12 08:38:26 +0200

    Added DOM function and better declared arrays

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function getComputerChoice () {
-    choices = ['rock', 'paper', 'scissors'];
+const winner = ['rockscissors', 'paperrock', 'scissorspaper'];
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return 'It\'s a Tie!';
-    }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) return 'It\'s a Tie!';
     let playerWin = winner.includes(playerSelection + computerSelection);
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
     computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
     let s = playerWin ? `You Win! ${playerSelection} beats ${computerSelection}` : `You Lose! ${computerSelection} beats ${playerSelection}`;
-
     return s;
 }
 
-function game () {
+function game() {
     playerScore = 0;
     cpuScore = 0;
 
@@ -23,6 +23,13 @@ function game () {
     buttons.forEach(button => {
         button.onclick = () => console.log(playRound(button.dataset.rps, getComputerChoice()));
     });
+}
+
+function showRoundWinner(roundWinner) {
+    const resultsDiv = document.querySelector('#results-container');
+    const roundResultDiv = document.createElement('div');
+    roundResultDiv.textContent = roundWinner;
+    resultsDiv.appendChild(roundResultDiv);
 }
 
 function getWinner(playerScore, cpuScore) {
@@ -36,6 +43,3 @@ function getWinner(playerScore, cpuScore) {
         return `It's a Tie! Both Player and CPU achieved a score of ${playerScore}!`;
     }
 }
-
-winner = ['rockscissors', 'paperrock', 'scissorspaper'];
-console.log(game());
